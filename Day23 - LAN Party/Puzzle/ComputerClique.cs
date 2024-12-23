fc698faa440e4411df85996575859f5a7fd3e3de
Mateusz Piotr Moruś <mpmorus@gmail.com>
2024-12-23 14:58:39 +0100

    Format code

--- Day23 - LAN Party/Puzzle/ComputerClique.cs
+++ Day23 - LAN Party/Puzzle/ComputerClique.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode.Year2024.Day23.Puzzle;
 internal sealed class ComputerClique(params IEnumerable<Computer> computers)
     : IReadOnlyCollection<Computer>
 {
-    private readonly HashSet<Computer> _computers = [..computers];
+    private readonly HashSet<Computer> _computers = [.. computers];
 
     public IReadOnlySet<Computer> Set => _computers; // For optimized set operations
     public int Count => _computers.Count;
